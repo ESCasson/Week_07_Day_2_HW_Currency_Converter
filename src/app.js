581b140b27a31884f65a2353096619085e426cc0
Ellie <cassona08@gmail.com>
2019-12-18 08:51:44 +0000

    adds first part of extension

--- src/app.js
+++ src/app.js
@@ -10,6 +10,9 @@ document.addEventListener('DOMContentLoaded', () => {
       amount: 0,
       calc: "from",
       conversionRate: 1,
+      multiAmount: 0,
+      multiConversionRateFrom: 1,
+      multiConversionRateTo: 1,
     },
 
     computed: {
@@ -22,6 +25,11 @@ document.addEventListener('DOMContentLoaded', () => {
         return value.toFixed(2)
         }
       },
+      multiCalculation: function(){
+        const value = (this.multiAmount / this.multiConversionRateFrom) *
+        this.multiConversionRateTo
+        return value.toFixed(2)
+      }
     },
 
     mounted(){
